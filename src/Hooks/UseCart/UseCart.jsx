af335ffeb0e1124c1b1be84fcf084231642eb2ec
safinahmedhasan <safinahmedhaasan@gmail.com>
2023-06-10 12:19:49 +0600

    added auth

--- src/Hooks/UseCart/UseCart.jsx
+++ src/Hooks/UseCart/UseCart.jsx
@@ -1,17 +1,27 @@
 import { useQuery } from '@tanstack/react-query'
 import { useContext } from 'react';
 import { AuthContext } from '../../providers/AuthProvider';
+import useAxiosSecure from './useAxiosSecure';
 const UseCart = () => {
-    const { user } = useContext(AuthContext)
+    const { user } = useContext(AuthContext);
+    const token = localStorage.getItem('access-token');
+    // const [axiosSecure] = useAxiosSecure();
 
 
 
-    const { refetch, data:cart = [] } = useQuery({
-        queryKey: ['carts' , user?.email],
+    const { refetch, data: cart = [] } = useQuery({
+        queryKey: ['carts', user?.email],
         queryFn: async () => {
-            const response = await fetch(`http://localhost:5000/carts?email=${user?.email}`)
+            const response = await fetch(`http://localhost:5000/carts?email=${user?.email}`, { headers: {
+                authorization: `bearer ${token}`
+            }})
             return response.json();
         },
+        // queryFn: async () => {
+        //     const response = await axiosSecure(`/carts?email=${user?.email}`)
+        //     console.log('res from axios', response);
+        //     return response.data;
+        // },
     })
     return [cart, refetch]
 
